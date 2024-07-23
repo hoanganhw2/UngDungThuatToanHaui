9394ab0127124dbbb2cce6ce27f0e0fabbcb9afb
phihoanganh23 <97159029+hoanganhw2@users.noreply.github.com>
2024-07-23 17:42:45 +0700

    Update onTX1_CongViec.cpp

--- onTX1_CongViec.cpp
+++ onTX1_CongViec.cpp
@@ -1,3 +1,34 @@
+/*
+Bài tập 5: Cho danh sách c gồm n công việc cần thực hiện trong ngày của một công ty, 
+thông tin về mỗi công việc gồm mã công việc, thời gian bắt đầu thực hiện, thời gian cần 
+thiết để thực hiện công việc (số phút hoặc số giờ cần thiết để thực hiện công việc). Cho 
+danh sách L gồm tên của n nhân viên.
+- Thiết kế thuật toán A1 để hiển thị ra màn hình tất cả các công việc trong danh 
+sách c theo thứ tự ngược lại bằng phương pháp đệ quy.
+- Thiết kế thuật toán A2 để đếm và trả về số công việc có thời gian thực hiện không 
+quá 30 phút (hoặc 0.5 giờ) trong danh sách b bằng chiến lược chia để trị.
+- Thiết kế thuật toán A3 để liệt kê tất cả các phương án khác nhau để giao n công 
+việc trong danh sách b cho n nhân viên trong danh sách L bằng phương pháp 
+quay lui.
+- Viết chương trình thực hiện.
+a) Khởi tạo số nguyên dương n sao cho 6 ≤ n ≤ 10. Khởi tạo danh sách c gồm 
+n công việc và khởi tạo danh sách nhân viên L (không nhập dữ liệu từ bàn 
+phím, dữ liệu có tính thực tiễn).
+b) Áp dụng thuật toán A1 để hiển thị danh sách các công việc trong danh 
+sách c theo thứ tự ngược lại.
+c) Áp dụng thuật toán A2 để đếm số công việc trong danh b có thời gian thực 
+hieenh không quá 30 phút, hiển thị kết quả (nếu có).
+d) Áp dụng thuật toán A3 để hiển thị tất cả các phương án khác nhau để giao 
+n công việc trong danh sách b cho n nhân viên trong danh sách L, mỗi 
+phương án hiển thị danh sách gồm tên công việc – tên nhân viên tương 
+ứng
+
+
+
+
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std ;
 
