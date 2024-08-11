ff639debd75e54dedee330984c5cd6dd90597a87
phihoanganh23 <97159029+hoanganhw2@users.noreply.github.com>
2024-08-12 06:44:59 +0700

    Update thamlam.cpp

--- thamlam.cpp
+++ thamlam.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std ;
-
+/*Bài tập 1: Chiến lược tham lam
+Input:
+- Cho một số tiền p và một danh sách d gồm n chiếc quạt bàn khác nhau về giá bán, 
+thông tin về mỗi chiếc quạt bàn gồm: tên hãng sản xuất, màu sắc và giá bán.
+Output:
+- Tính c và s lần lượt là số lượng và danh sách nhiều nhất các quạt bàn có thể mua được 
+từ danh sách d với số tiền p.
+Yêu cầu thực hiện: 
+- Thiết kế thuật toán G1 để tính và trả về c và s theo chiến lược tham lam.
+- Viết chương trình thực hiện:
+o Khai báo cấu trúc dữ liệu của bài toán.
+o Khởi tạo p (0 < p < 1200000), n và danh sách d (5 < n < 12). Yêu cầu dữ liệu 
+thể hiện được tính thực tiễn.
+o Cài đặt hàm F1 biểu diễn thuật toán G1.
+o Sử dụng thuật toán G1 để tính và trả về c và s. Hiển thị các kết quả, trong đó 
+mỗi chiếc quạt cần hiển thị tên hãng sản xuất và giá bán */
 struct quat{
 	string name ;
 	string color;
