df92bd495e558ccb8832d376607420cf155132d4
phihoanganh23 <97159029+hoanganhw2@users.noreply.github.com>
2024-08-12 09:21:49 +0700

    Update quyhoachdong.cpp

--- quyhoachdong.cpp
+++ quyhoachdong.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+/*
+Bài 4: Chiến lược quy hoạch động
+Input:
+- Cho một chiếc túi có kích thước s và một danh sách d gồm n chiếc điện thoại khác 
+nhau, thông tin về mỗi điện thoại gồm nhãn hiệu, kích thước và giá bán.
+Output:
+- Tính x và a lần lượt là số lượng và danh sách những chiếc điện thoại trong danh sách 
+d cần đưa vào chiếc túi để để được tổng giá bán lớn nhất mà không vượt quá kích thước 
+s của chiếc túi.
+Yêu cầu thực hiện
+- Thiết kế thuật toán Q1 để tính và trả về x và a.
+- Viết chương trình thực hiện:
+- Khởi tạo n và danh sách d (5 ≤ n ≤ 10).
+- Cài đặt hàm biểu diễn thuật toán Q1.
+- Sử dụng thuật toán Q1 để tính và trả về x và a. Hiển thị kết quả (mỗi điện thoại hiển 
+thị nhãn hiệu, giá bán).
+*/
 struct phone {
     string name;
     int size;
